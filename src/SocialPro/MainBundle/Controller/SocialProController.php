67551dbebfef16c3252bc9528346441f6c583433
FayrouzOuerghi <fayrouz.ouerghi@esprit.tn>
2017-02-02 23:54:45 +0100

    "preparation du profil du user"

--- src/SocialPro/MainBundle/Controller/SocialProController.php
+++ src/SocialPro/MainBundle/Controller/SocialProController.php
@@ -10,12 +10,19 @@ class SocialProController extends Controller
     {
         return $this->render('SocialProMainBundle:SocialPro:index.html.twig');
     }
+
     public function mainpageAction()
     {
         return $this->render('SocialProMainBundle:SocialPro:mainpage.html.twig');
     }
+
     public function profile2Action()
     {
         return $this->render('SocialProMainBundle:SocialPro:profile.html.twig');
+    }
+
+    public function parametresprofilAction()
+    {
+        return $this->render('SocialProMainBundle:SocialPro:parametresprofil.html.twig');
     }
 }
