16ac698b2a7c098af46100ef5f1d39609b3d3417
Gouzayz <aymen.guezmir@esprit.tn>
2017-02-05 16:54:21 +0100

    fonctionnalité ajout projet en marche, très basique, pas de controle

--- src/SocialPro/ProjectBundle/Controller/ProjectController.php
+++ src/SocialPro/ProjectBundle/Controller/ProjectController.php
@@ -2,7 +2,12 @@
 
 namespace SocialPro\ProjectBundle\Controller;
 
+use SocialPro\ProjectBundle\Entity\Projet;
+use SocialPro\ProjectBundle\Form\ProjetType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class ProjectController extends Controller
 {
@@ -14,9 +19,28 @@ class ProjectController extends Controller
     {
         return $this->render();
     }
-    public function addAction()
+    public function addAction(Request $request)
     {
-        return $this->render();
+        $project = new Projet();
+        $form=$this->createForm(ProjetType::class,$project);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted()){
+            $session= $this->getUser()->getId();
+            $project->setIdInd($session);
+            $em=$this->getDoctrine()->getManager();
+
+            $em->persist($project);
+            $em->flush();
+
+            return new Response('produit ajouté');
+
+        }
+
+
+        $formView=$form->createView();
+
+        return $this->render('@Project/Project/ajout.html.twig', array('form'=>$formView));
     }
     public function foundsAction($id)
     {
---
+++ src/SocialPro/ProjectBundle/Form/ProjetType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace SocialPro\ProjectBundle\Form;
+
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProjetType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('idInd', HiddenType::class)->add('description', TextareaType::class)->add('cout', MoneyType::class)->add('domaine', TextType::class)        ;
+    }
+    //
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'SocialPro\ProjectBundle\Entity\Projet'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'socialpro_projectbundle_projet';
+    }
+
+
+}
